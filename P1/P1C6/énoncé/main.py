3d3d9bfbe676b2a923a0670732354a679a0298fb
Lawk16 <leonscovisch@gmail.com>
2024-07-25 10:56:52 +0000

    Update main.py

--- P1/P1C6/énoncé/main.py
+++ P1/P1C6/énoncé/main.py
@@ -1,1 +1,21 @@
 # Écrivez votre code ici !
+# Création de la liste nommée fruits contenant "pomme","banane" et "orange"
+fruits = ["pomme", "banane", "orange"]
+print(fruits)
+
+# Ajout de "kiwi" à la liste fruits
+fruits.append("kiwi")
+print(fruits)
+
+# Modification du deuxième élément de la liste fruits en "ananas"
+fruits[1] = "ananas"
+print(fruits)
+
+# Affichage de la longueur de la liste fruits
+print(len(fruits))
+
+# Trie de la liste fruits par ordre alphabétique*
+fruits.sort()
+
+# Affichage de la liste
+print(fruits)
