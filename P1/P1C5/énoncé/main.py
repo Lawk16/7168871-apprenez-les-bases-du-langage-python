6e299ee044a20b825b747a4b21af7610924eafe8
Lawk16 <leonscovisch@gmail.com>
2024-07-25 00:36:06 +0000

    Update main.py

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,19 @@
 # Écrivez votre code ici !
+#Déclaration des variables
+nom = "Lawk"
+age = 21
+taille = 1.75
+est_etudiant = True
+
+#Affichage des variables précédemment définies
+print(f"Nom : {nom}\n")
+print(f"Age : {age} ans\n")
+print(f"Taille : {taille}\n")
+print(f"Est étudiant : {est_etudiant}\n")
+
+#Utilisation de la fonction type avec la fonction print
+#pour afficher les types de variables précédemment définies
+print(f"Type nom : {type(nom)}\n")
+print(f"Type age : {type(age)}\n")
+print(f"Type taille : {type(taille)}\n")
+print(f"Type est étudiant : {type(est_etudiant)}\n")
