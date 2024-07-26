4edd19834e756bc41271c668fc9b8f732d1d9aee
Lawk16 <leonscovisch@gmail.com>
2024-07-26 03:02:18 +0000

    Update main.py

--- P1/P1C7/énoncé/main.py
+++ P1/P1C7/énoncé/main.py
@@ -1,1 +1,24 @@
 # Écrivez votre code ici !
+# Création du dictionnaire fruits
+fruits = {
+  "pomme" : "rouge" ,
+  "banane" : "jaune" ,
+  "orange" : "orange"
+}
+
+# Ajout de la clé "kiwi" avec la valeur "vert"
+fruits["kiwi"] = "vert"
+
+# Stockage de la valeur correspondant à la clé banane dans une variable appellée banane_couleur
+banane_couleur = fruits["banane"]
+print("La banne est",banane_couleur,)
+
+# Modification de la valeur associée à "pomme" pour "vert"
+fruits ["pomme"] = "vert"
+
+# Suppression de la clé "banane" du dictionaire et affichage
+fruits.pop("banane")
+print(fruits)
+
+# Affichage des clés restants
+print(fruits.keys()) 
